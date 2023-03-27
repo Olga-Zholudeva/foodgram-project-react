31239a64333978b2c86f2919add201f22a3957e9
Ольга Жолудева <joludeva.olga@yandex.ru>
2023-03-27 09:18:57 +0300

    Сделала поле email в модели User регистронезависимым

--- backend/foodgram_project_backend/api/serializers.py
+++ backend/foodgram_project_backend/api/serializers.py
@@ -88,6 +88,7 @@ class CreateReceptSerializer(serializers.ModelSerializer):
         instance.cooking_time = validated_data.get(
             'cooking_time', instance.cooking_time
         )
+        instance.ingredients = validated_data('ingredients', instance.ingredients)
         ReceptTabel.objects.filter(recept=instance).delete()
         tags = validated_data.pop('tags')
         ingredients = validated_data.pop('ingredients')
